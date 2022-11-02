8063f70890586f6969d37a3fa339b49ae733f620
@preetsimon <preetsimon@gmail.com>
2022-11-01 17:24:21 -0700

    Expense tracker frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import './styles/App.scss';
+import {Routes, Route} from "react-router-dom";
+
+// Components
+import PageContainer from './components/Containers/PageContainer';
+import Navbar from './components/Navbar/Navbar';
+import MobileNavbar from './components/Navbar/MobileNavbar';
+function App() {
+  return (
+    <div className="App">
+    <PageContainer>
+      <Navbar/>
+      <div className='mobileMenu'>
+        <MobileNavbar/>
+      </div>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </PageContainer>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar/ListItemLink.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import styles from "../../styles/NavBar/ListItemLink.module.scss";
+
+const ListItemLink = ({url, children, clickHandler, optionClass}) => {
+  return (
+    <li className={`${styles.listItem} ${optionClass}`}
+    onClick = {clickHandler}
+    >
+       <NavLink to ={`/${url}`}
+       className= {({isActive})=> (isActive ? styles.active : undefined )}
+       >
+        {children}
+        </NavLink> 
+    </li>
+  )
+}
+
+ListItemLink.defaultProps = {
+    url : "",
+    optionClass: undefined,
+}
+
+export default ListItemLink
